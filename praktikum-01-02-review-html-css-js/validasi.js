7224b3270ebb1895eaedd0dc5f8a5ef8dea9d587
Apriliani210401 <nurindah01apriliani@gmail.com>
2022-10-24 08:29:13 +0800

    tugas ke-2

---
+++ praktikum-01-02-review-html-css-js/validasi.js
@@ -0,0 +1,11 @@
+function validasi ()
+{
+    let txtKataKunci = document.getElementById("txtKataKunci");
+    if (txtKataKunci.value == "")
+    {
+        alert("Anda Harus Mengisi Textbox Sembarang Kata");
+    }
+    else{
+        alert("Anda Ingin Mencari Dengan Kata Kunci = "+txtKataKunci.value);
+    }
+}
