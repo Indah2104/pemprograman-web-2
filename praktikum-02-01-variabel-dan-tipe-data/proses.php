213c9182a1285b26684f1c83e0c02541d05cbe70
Apriliani210401 <nurindah01apriliani@gmail.com>
2022-10-31 10:08:37 +0800

    tugas ketiga

---
+++ praktikum-02-01-variabel-dan-tipe-data/proses.php
@@ -0,0 +1,10 @@
+<?php
+
+if ($txtnim = isset ($_POST["txtnim"]) ? $_POST["txtnim"] : "" AND $txtnama = isset ($_POST["txtnama"]) ? $_POST["txtnama"] : "" AND $txtpstudi = isset ($_POST["txtpstudi"]) ? $_POST["txtpstudi"] : ""){
+    echo "<p>Hai, perkenalkan nama saya adalah : ".$txtnama." dengan NIM : ".$txtnim.". Saya adalah mahasiswa dari program studi :".$txtpstudi;
+}
+else
+{
+   echo("Isi Semua Data,'<a href=input.html>Klik Di Sini</a>' untuk kembali" );
+}
+?>
