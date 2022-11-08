22f5fd83e3923d2bb2d450f90b12d123aeccf635
Apriliani210401 <nurindah01apriliani@gmail.com>
2022-11-08 13:56:19 +0800

    tugas array

--- praktikum-02-02-array/array_2.php
+++ praktikum-02-02-array/array_2.php
@@ -14,14 +14,14 @@
             <td width = 400px >Andi, Budi, Wati, Gunawan, Baco, Becce, Fatma</td>
         </tr>
         <tr>
-            <td bgcolor = "yellow" >Programmer Phyten </td>
+            <td bgcolor = "yellow" >Programmer Phyton </td>
             <td>Andi, Fatma, Fadli, Haris, Baco</td>
         </tr>
     </table>
     <br/>
     <?php
         $program_php = array ("Andi", "Budi", "Wati" , "Gunawan" , "Baco" , "Becce" , "Fatma");
-        $program_phyten = array ("Andi" , "Fatma" , "Fadli" , "Haris" , "Baco");
+        $program_phyton = array ("Andi" , "Fatma" , "Fadli" , "Haris" , "Baco");
         
         echo "Daftar mahasiswa yang masuk kelompok Programmer PHP dengan nama diurutkan secara Ascending : ";
         echo "<ul>";
@@ -34,15 +34,15 @@
 
         echo "Daftar mahasiswa yang masuk kelompok Programmer Python dengan nama diurutkan secara Descending :";
         echo "<ul>";
-        arsort($program_phyten);
-        foreach ($program_phyten as $data)
+        arsort($program_phyton);
+        foreach ($program_phyton as $data)
         {
             echo "<li>".$data."</li>";
         }
         echo "</ul>";
 
         echo "Daftar mahasiswa yang masuk kelompok Programmer PHP tapi tidak termasuk kelompok Programmer Python : ";
-        $bukan_python = array_diff ($program_php, $program_phyten);
+        $bukan_python = array_diff ($program_php, $program_phyton);
         echo "<ul>";
         asort($bukan_python);
         foreach ($bukan_python as $data)
@@ -53,7 +53,7 @@
 
 
         echo "Daftar mahasiswa yang masuk kelompok Programmer Python tapi tidak termasuk kelompok Programmer PHP : ";
-        $bukan_php = array_diff ($program_phyten, $program_php);
+        $bukan_php = array_diff ($program_phyton, $program_php);
         echo "<ul>";
         asort($bukan_php);
         foreach ($bukan_php as $data)
@@ -63,18 +63,18 @@
         echo "</ul>";
 
         echo "Daftar mahasiswa yang masuk kelompok Programmer PHP dan Programmer Python sekaligus : ";
-        $php_dan_pythen = array_intersect ($program_phyten, $program_php);
+        $php_dan_python = array_intersect ($program_phyton, $program_php);
         echo "<ul>";
-        asort($php_dan_pythen);
-        foreach ($php_dan_pythen as $data)
+        asort($php_dan_python);
+        foreach ($php_dan_python as $data)
         {
             echo "<li>".$data."</li>";
         }
         echo "</ul>";
 
 
-        echo "Daftar seluruh mahasiswa yang masuk kelompok Programmer PHP atau Programmer Python.Satukan terlebih dahulu data dari kedua variabel array dengan teknik merge : ";
-        $program = array_merge ($program_php, $program_python);
+        echo "Daftar seluruh mahasiswa yang masuk kelompok Programmer PHP atau Programmer Python. Satukan terlebih dahulu data dari kedua variabel array dengan teknik merge : ";
+        $program = array_merge ($program_php, $program_phyton);
         echo "<ul>";
         asort($program);
         foreach ($program as $data)
@@ -86,7 +86,6 @@
 
         echo "Lengkapi file array_2 php dengan perintah untuk menambahkan nama Anda ke dalam variabe programmer_php dengan menggunakan perintah array push().";
         array_push($program_php, "Nurlinda Apriliani");
-        echo "<br>Bagian 2 : <br>";
         echo "<ul>";
         $countProgramPhp = count($program_php);
         for($i=0; $i < $countProgramPhp; $i++)
